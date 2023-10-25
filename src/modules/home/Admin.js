b3f3f2ec60e0f6be69981a3ef467d2f920ea9fc8
cuneiform-ajinkya <ajinkya.pawar@thecuneiform.com>
2023-10-25 11:23:18 +0530

    second change

--- src/modules/home/Admin.js
+++ src/modules/home/Admin.js
@@ -1,125 +1,214 @@
-import { StyleSheet, Text, View, TouchableOpacity, TextInput, Button, Alert } from 'react-native'
-import React, { useState } from 'react'
+import {
+  Alert,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React from "react";
 import LinearGradient from "react-native-linear-gradient";
+import { useRef } from "react";
+import { useState } from "react";
 
-const Admin = ({ navigation }) => {
+const Admin = () => {
+  const et1 = useRef();
+  const et2 = useRef();
+  const et3 = useRef();
+  const et4 = useRef();
 
-  const generateOTP = () => {
-    const otp = Math.floor(100000 + Math.random() * 900000);
-    return otp.toString();
+  const [f1, setF1] = useState();
+  const [f2, setF2] = useState();
+  const [f3, setF3] = useState();
+  const [f4, setF4] = useState();
+
+  const generateOtpfunc = () => {
+    const min = 1000;
+    const max = 9999;
+
+    const otp = Math.floor(Math.random() * (max - min + 1)) + min;
+    let enteredOtp = f1 + f2 + f3 + f4;
+
+
+    console.log(otp);
   };
+  const validateOtp = (otp , enteredOtp) => {
 
-  const [otp, setOtp] = useState(['', '', '', '', '', '']);
-
-  const handleOtpChange = (value, index) => {
-    const newOtp = [...otp];
-    newOtp[index] = value;
-    setOtp(newOtp);
-  };
-
-  const checkOTP = () => {
-    const enteredOTP = otp.join(''); 
-
-    if (enteredOTP === generatedOTP) {
-      Alert.alert('OTP Matched');
+    if (enteredOtp == otp) {
+      Alert.alert("Otp Matched");
     } else {
-      Alert.alert('OTP Doesn\'t Match');
+      Alert.alert("Wrong otp");
     }
   };
 
-  const generatedOTP = generateOTP();
-
   return (
-    <LinearGradient
-      colors={["darkorchid", "floralwhite"]}
-      style={{ flex: 1 }}
-    >
+    <LinearGradient colors={["darkorchid", "floralwhite"]} style={{ flex: 1 }}>
       <View style={styles.container}>
-        <TouchableOpacity>
-          <Button
-            title='Get OTP'
-            color='#841584'
-            onPress={() => { console.log(generateOTP()) }} />
-        </TouchableOpacity>
-        <View style={styles.mainText}>
-          <View style={styles.otpContainer}>
-            {[...Array(6)].map((_, index) => (
-              <TextInput
-                key={index}
-                style={styles.otpInput}
-                keyboardType="numeric"
-                maxLength={1}
-                value={otp[index]}
-                onChangeText={(value) => handleOtpChange(value, index)}
-              />
-            ))}
-          </View>
+        <View>
+          <Text style={styles.otpVerify}>Otp Verification</Text>
         </View>
-        <View style={styles.bottomButtons}>
-          <TouchableOpacity onPress={() => checkOTP()} style={styles.verifyButton}>
-            <Text style={styles.text2}>Verify OTP</Text>
+        <View>
+          <TouchableOpacity
+            onPress={generateOtpfunc}
+            style={styles.generateOtp}
+          >
+            <Text style={styles.buttonText}>Generate Otp</Text>
           </TouchableOpacity>
-          <TouchableOpacity onPress={() => navigation.navigate('Profile')} style={styles.logoutButton}>
-            <Text style={styles.text2}>Logout</Text>
+        </View>
+        <View style={styles.otpView}>
+          <TextInput
+            ref={et1}
+            value={f1}
+            style={[
+              styles.textInput,
+              { borderColor: (f1 && f1.length >= 1) >= 1 ? "red" : "black" },
+            ]}
+            keyboardType="number-pad"
+            maxLength={1}
+            onChangeText={(txt) => {
+              setF1(txt);
+              if (txt.length >= 1) {
+                et2.current.focus();
+              } else if (txt.length < 1) {
+                et1.current.focus();
+              }
+            }}
+          ></TextInput>
+          <TextInput
+            ref={et2}
+            value={f2}
+            style={[
+              styles.textInput,
+              { borderColor: (f2 && f2.length >= 1) >= 1 ? "red" : "black" },
+            ]}
+            keyboardType="number-pad"
+            maxLength={1}
+            onChangeText={(txt) => {
+              setF2(txt);
+              if (txt.length >= 1) {
+                et3.current.focus();
+              } else if (txt.length < 1) {
+                et1.current.focus();
+              }
+            }}
+          ></TextInput>
+          <TextInput
+            ref={et3}
+            value={f3}
+            style={[
+              styles.textInput,
+              { borderColor: (f3 && f3.length >= 1) >= 1 ? "red" : "black" },
+            ]}
+            keyboardType="number-pad"
+            maxLength={1}
+            onChangeText={(txt) => {
+              setF3(txt);
+              if (txt.length >= 1) {
+                et4.current.focus();
+              } else if (txt.length < 1) {
+                et2.current.focus();
+              }
+            }}
+          ></TextInput>
+          <TextInput
+            ref={et4}
+            value={f4}
+            style={[
+              styles.textInput,
+              { borderColor: (f4 && f4.length >= 1) >= 1 ? "red" : "black" },
+            ]}
+            keyboardType="number-pad"
+            maxLength={1}
+            onChangeText={(txt) => {
+              setF4(txt);
+              if (txt.length >= 1) {
+                et4.current.focus();
+              } else if (txt.length < 1) {
+                et3.current.focus();
+              }
+            }}
+          ></TextInput>
+        </View>
+
+        <View>
+          <TouchableOpacity
+            onPress={validateOtp}
+            disabled={
+              f1 !== "" && f2 !== "" && f3 !== "" && f4 !== "" ? false : true
+            }
+            style={[
+              styles.verifyOtp,
+              {
+                backgroundColor:
+                  f1 !== "" && f2 !== "" && f3 !== "" && f4 !== ""
+                    ? "lightyellow"
+                    : "gray",
+              },
+            ]}
+          >
+            <Text style={styles.buttonText}>Verify OTP</Text>
           </TouchableOpacity>
         </View>
       </View>
     </LinearGradient>
-  )
-}
+  );
+};
 
-export default Admin
+export default Admin;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+    marginTop: "10%",
   },
-  text1: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    color: '#000'
+  otpVerify: {
+    fontSize: 22,
+    fontWeight: "700",
+    marginTop: 100,
+    alignSelf: "center",
+    color: "#000",
   },
-  text2: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    color: '#000'
+  otpView: {
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row",
+    marginTop: 50,
+    marginRight: 35,
   },
-  bottomButtons: {
-    position: 'absolute',
-    bottom: 0,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    width: '100%',
-    paddingHorizontal: 16,
-    marginBottom: 16
-  },
-  verifyButton: {
-    backgroundColor: '#841584',
-    paddingVertical: 10,
-    paddingHorizontal: 20,
+  textInput: {
+    width: 40,
+    height: 40,
+    borderWidth: 0.5,
     borderRadius: 10,
-    marginRight: 10
+    marginLeft: 10,
+    textAlign: "center",
+    fontSize: 18,
+    fontWeight: "700",
   },
-  logoutButton: {
-    backgroundColor: '#841584',
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    borderRadius: 10
+  verifyOtp: {
+    flexDirection: "row",
+    height: 55,
+    width: 120,
+    backgroundColor: "lightyellow",
+    borderRadius: 20,
+    alignSelf: "center",
+    marginTop: 50,
+    justifyContent: "center",
+    alignItems: "center",
   },
-  otpContainer: {
-    flexDirection: 'row',
-    marginBottom: 30
+  buttonText: {
+    color: "black",
+    fontSize: 15,
   },
-  otpInput: {
-    fontSize: 30,
-    height: 50,
-    width: 50,
-    textAlign: 'center',
-    borderWidth: 2,
-    borderRadius: 10,
-    borderColor: 'black',
-    marginHorizontal: 5
+  generateOtp: {
+    flexDirection: "row",
+    height: 55,
+    width: 120,
+    backgroundColor: "lightyellow",
+    borderRadius: 20,
+    alignSelf: "center",
+    marginTop: 50,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
